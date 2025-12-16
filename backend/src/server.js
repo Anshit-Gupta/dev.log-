3c2df58be4c79dc14a550c3f4d51608dc6b36362
Anshit-Gupta <anshitgupta777@gmail.com>
2025-12-16 21:54:27 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import notesRoutes from './routes/notesRoutes.js'
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
@@ -9,12 +10,17 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app =express(); 
+const __dirname=path.resolve();
 
 // **order of middleware is important***
 
-app.use(cors({
+if(process.env.NODE_ENV !=="production")
+{
+   app.use(cors({
     origin:"http://localhost:5173",
-}));
+    }));
+}
+
 
 app.use(express.json());//middleware that converts json to node readable js object or will parse JSON bodies:req.body
 
@@ -22,6 +28,18 @@ app.use(express.json());//middleware that converts json to node readable js obje
 app.use(rateLimiter); // to implement rate limiting
 
 app.use("/api/notes",notesRoutes);
+
+// path 
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+
+if(process.env.NODE_ENV ==="production")
+{
+   app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
+
 
 connectDB().then(()=>{   // this is better , after my DB is connected then start the server as like if my db connection fail what is the point of starting my server
 app.listen(process.env.PORT || 5001,()=>{
